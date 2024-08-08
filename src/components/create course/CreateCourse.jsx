e3ac631b139d3e2df65abe08b73fec2f1ef22204
Avi <avimahalingpure10@gmail.com>
2024-08-08 14:26:57 +0530

    bug fixes

--- src/components/create course/CreateCourse.jsx
+++ src/components/create course/CreateCourse.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./CreateCourse.css";
 import { useEffect } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CreateCourse = () => {
   const [courseName, setCourseName] = useState("");
   const [courseDescription, setCourseDescription] = useState("");
   const [coursePrice, setCoursePrice] = useState(0);
   const [thumbnail, setThumbnail] = useState(null);
+  const fileInputRef = useRef(null);
 
   const url = "http://localhost:5000";
   const token = localStorage.getItem("token");
@@ -29,14 +32,21 @@ const CreateCourse = () => {
         },
         formData,
       });
+
+      toast.success("course created sucessfully");
     } catch (error) {
-      console.log(error);
+      toast.error(error.response.data.message);
     }
 
     setThumbnail(null);
     setCourseName("");
     setCourseDescription("");
     setCoursePrice(0);
+  };
+
+  const handleUploadFile = (e) => {
+    e.preventDefault();
+    fileInputRef.current.click();
   };
 
   const handleFileChange = (e) => {
@@ -46,6 +56,7 @@ const CreateCourse = () => {
 
   return (
     <div className="create-course">
+      <ToastContainer />
       <form onSubmit={(e) => createCourse(e)}>
         <input
           value={courseName}
@@ -69,11 +80,25 @@ const CreateCourse = () => {
           placeholder="course price"
         />
 
-        <div style={{ display: "flex", gap: "10px" }}>
+        <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
           <p htmlFor="fileInput" style={{ margin: "0px" }}>
             Choose Thumbnail :
           </p>
-          <input type="file" name="thumbnail" onChange={handleFileChange} />
+          <input
+            type="file"
+            name="thumbnail"
+            accept="image/*"
+            style={{ display: "none" }}
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
+
+          <button
+            className="choose-file-btn"
+            onClick={(e) => handleUploadFile(e)}
+          >
+            Choose file
+          </button>
         </div>
         <button type="submit">create</button>
       </form>
